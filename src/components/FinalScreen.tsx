430f6d4bd1a8219d6690113e26c0fe3044c97a0c
Slava <slasla@gmail.com>
2023-04-12 13:15:44 +0300

    publish to gh pages

--- src/components/FinalScreen.tsx
+++ src/components/FinalScreen.tsx
@@ -13,9 +13,9 @@ function FinalScreen({ score, onStart }: FinalScreenProps) {
   return (
     <div className="welcome-screen" style={BackgroundStyle}>
         <img src={hand} alt="Welcoming Hand" className="welcome-screen__image" />
-      <div className="welcome-screen__container">
+      <div className="final-screen__container">
         <h3 className="final-screen__text">Total score:</h3>
-        <h1 className="welcome-screen__title"><span>${score} earned</span></h1>
+        <h1 className="final-screen__title"><span>${score} earned</span></h1>
         <button className="welcome-screen__button" onClick={onStart}>
           Try Again
         </button>
