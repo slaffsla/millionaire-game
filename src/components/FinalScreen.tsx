e7bb8651dfa7f366559e2707996ca5d6bf3873b6
Slava <slasla@gmail.com>
2023-04-17 16:07:31 +0300

    css touches

--- src/components/FinalScreen.tsx
+++ src/components/FinalScreen.tsx
@@ -12,11 +12,11 @@ function FinalScreen({ score, onStart }: FinalScreenProps) {
   
   return (
     <div className="welcome-screen" style={BackgroundStyle}>
-        <img src={hand} alt="Welcoming Hand" className="welcome-screen__image" />
-      <div className="final-screen__container">
+        <img src={hand} alt="Welcoming Hand" className="welcome-screen__image final-screen__image" />
+      <div className="welcome-screen__container final-screen__container">
         <h3 className="final-screen__text">Total score:</h3>
-        <h1 className="final-screen__title"><span>${score} earned</span></h1>
-        <button className="welcome-screen__button" onClick={onStart}>
+        <h1 className="welcome-screen__title final-screen__title"><span>${score} earned</span></h1>
+        <button className="welcome-screen__button final-screen__button" onClick={onStart}>
           Try Again
         </button>
       </div>
